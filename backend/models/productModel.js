aa2d375d49fb3a1ee6872c97091e911fee522f33
kailash talreja <kailashtalreja30@gmail.com>
2021-05-16 23:05:41 +0530

    Create Review Endpoint Completed

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -12,7 +12,6 @@ const reviewSchema = mongoose.Schema(
     },
     comment: {
       type: String,
-      required: true,
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,23 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 const app = express();
 
-app.use(express.json());
-
 dotenv.config();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
 
 connectDB();
 app.get('/', (req, res) => {
